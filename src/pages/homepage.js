fe9f7642108f6b80ebf42588f55026581de352f3
Gwen <gwenael.girod@gmail.com>
2020-11-19 20:47:54 +0100

    Homepage image

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -5,6 +5,7 @@ import {Button} from 'react-bootstrap';
 import withRouter from "react-router-dom/es/withRouter";
 import {connect} from "react-redux";
 import {ACTIONS_FORM} from "../actions/formActions";
+import generalImg from "../assets/general.png"
 
 const HomePage = (props) => {
 
@@ -17,6 +18,7 @@ const HomePage = (props) => {
     return <Layout title="Solify">
         <div className={styles.homepageContainer}>
             <h2>Welcome to Solify</h2>
+            <img src={generalImg} className={styles.image}/>
             <div className={styles.body}>
                 <div className={styles.text}>
                     <p>Find out what type of stress has affected your soil.</p>
